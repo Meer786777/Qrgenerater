cb5ea9a69d8c5d90efce472cccc14ec22f393f7f
Meer786777 <133601225+Meer786777@users.noreply.github.com>
2024-05-06 07:42:52 +0100

    QR generater
    works

---
+++ script.js
@@ -0,0 +1,17 @@
+let imgbox = document.getElementById('imgbox')
+let qrimage = document.getElementById('qrimage')
+let qrtext = document.getElementById('qrtext')
+function generateQR() {
+
+    if (qrtext.value.length > 0) {
+
+        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+        imgbox.classList.add("show-img")
+
+    } else {
+        qrtext.classList.add('error');
+        setTimeout(() => {
+            qrtext.classList.remove('error')
+        }, 1000)
+    }
+}
